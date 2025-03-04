5e96a9c1a977ca0beb0d7ac587576a7798420bbd
ashupathak7617 <ashupathak7617@gmail.com>
2025-03-04 19:11:14 +0530

    worked on seats view

--- db/migrate/20250218053249_create_shows.rb
+++ db/migrate/20250218053249_create_shows.rb
@@ -2,6 +2,7 @@ class CreateShows < ActiveRecord::Migration[7.2]
   def change
     create_table :shows do |t|
       t.datetime :show_time
+      t.datetime :end_time
       t.references :movie, null: false, foreign_key: true
       t.references :theater, null: false, foreign_key: true
       t.references :screen, null: false, foreign_key: true
---
+++ db/migrate/20250303121002_add_show_ref_to_bookings.rb
@@ -0,0 +1,5 @@
+class AddShowRefToBookings < ActiveRecord::Migration[7.2]
+  def change
+    add_reference :bookings, :show, null: false, foreign_key: true
+  end
+end
