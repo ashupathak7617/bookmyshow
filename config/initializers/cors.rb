0f2d08698b6663c0fc5f0490f1f8d069abf6d1d1
ashupathak7617 <ashupathak7617@gmail.com>
2026-06-26 12:54:16 +0530

    fix: update CORS for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "https://bookmyshow-frontend-h2z6.onrender.com",
-            "http://localhost:5173"
+            "http://localhost:5173",
+            "http://localhost:5174"
 
     resource "*",
       headers: :any,
